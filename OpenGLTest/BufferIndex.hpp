b84d94f1001f17d091af31a5668c9f0810ebad7e
Ryan M <ryanpmcn@gmail.com>
2024-05-28 16:21:54 -0400

    Rename IndexBufferSub to BufferIndexRange

--- OpenGLTest/BufferIndex.hpp
+++ OpenGLTest/BufferIndex.hpp
@@ -3,11 +3,11 @@
 #include <GLAD\gl.h>
 #include <vector>
 
-struct IndexBufferSub {
+struct BufferIndexSub {
 	GLsizeiptr subBufferSize;
 	GLintptr bufferOffset;
 
-	IndexBufferSub(std::vector<unsigned int> const& indexes, GLintptr offset) : subBufferSize(sizeof(unsigned int) * indexes.size()), bufferOffset(offset) {
+	BufferIndexSub(std::vector<unsigned int> const& indexes, GLintptr offset) : subBufferSize(sizeof(unsigned int) * indexes.size()), bufferOffset(offset) {
 
 	}
 };
@@ -23,7 +23,7 @@ class BufferIndex : public Buffer {
 		return bufferSize;
 	}
 public:
-	std::vector<IndexBufferSub> subBuffers;
+	std::vector<BufferIndexSub> subBuffers;
 	BufferIndex(std::vector<unsigned int> const& indexes) : subBuffers(), Buffer(sizeof(std::remove_reference_t<decltype(indexes)>::value_type) * indexes.size(), (void*)indexes.data()) {
 		subBuffers.emplace_back(indexes, 0);
 	}
@@ -47,6 +47,10 @@ public:
 	~BufferIndex() {
 
 	}
+
+	//bool ResetBuffer(std::vector<unsigned int> const& indexes) {
+	//
+	//}
 
 	friend VertexArray;
 };
